bb7bab08257360b230871d4a2a2effd7b9d912c3
Eric <ericbrolio@gmail.com>
2024-09-12 20:03:19 -0300

    Logica inicial

---
+++ CustoFuncionarioApp/Controllers/FuncionarioController.cs
@@ -0,0 +1,25 @@
+﻿using CustoFuncionarioApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustoFuncionarioApp.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Calcular(Funcionario funcionario)
+        {
+            if (ModelState.IsValid)
+            {
+                var custoFuncionario = funcionario.SalarioBase + funcionario.Beneficio - funcionario.Desconto;
+
+                ViewData["CustoFuncionario"] = custoFuncionario;
+            }
+
+            return View("Index", funcionario); // Retorna à view 'Index' com o modelo 'funcionario'
+        }
+    }
+}
