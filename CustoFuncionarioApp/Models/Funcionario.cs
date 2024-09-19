93880b3970bd35092ce59645f65289526ac8473a
Eric <ericbrolio@gmail.com>
2024-09-19 00:05:30 -0300

    Adicionado alguns mimus

--- CustoFuncionarioApp/Models/Funcionario.cs
+++ CustoFuncionarioApp/Models/Funcionario.cs
@@ -1,7 +1,10 @@
-﻿namespace CustoFuncionarioApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustoFuncionarioApp.Models
 {
     public class Funcionario
     {
+        [StringLength(30, ErrorMessage = "O nome pode ter no máximo 30 caracteres.")]
         public string Nome { get; set; }
         public decimal SalarioBase { get; set; }
         public decimal Desconto { get; set; }
--- CustoFuncionarioApp/obj/Debug/net8.0/CustoFuncionarioApp.AssemblyInfo.cs
+++ CustoFuncionarioApp/obj/Debug/net8.0/CustoFuncionarioApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CustoFuncionarioApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43886fee38dd0ad27820e7302e2015c466de5e9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustoFuncionarioApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustoFuncionarioApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
